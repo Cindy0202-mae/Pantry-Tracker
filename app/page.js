37784e129e36899727dc0eb0a11ee2cb7f655812
Cindy0202-mae <cindymaen17@gmail.com>
2024-08-01 15:27:54 +0900

    updated

--- app/page.js
+++ app/page.js
@@ -1,10 +1,32 @@
-'user client'
+'use client'
 import Image from "next/image";
-import {userState, userEffect} from 'react';
-import { Firestore } from "@/firebase";
+import {useState, useEffect} from 'react';
+import { firestore, Firestore } from "@/firebase";
 import { Box, Typography } from "@mui/material";
+import { collection, getDocs, query } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc)=>{
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
   return (
     <Box>
       <Typography variant="h1">Inventory Management</Typography>
