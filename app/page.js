986647db8ee1e686436189f0b61d02bf80a92cae
Cindy0202-mae <cindymaen17@gmail.com>
2024-08-02 15:18:23 +0900

    update

--- app/page.js
+++ app/page.js
@@ -79,31 +79,21 @@ export default function Home() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Add Item
-        </Typography>
-        <Stack width="100%" direction={'row'} spacing={2}>
-          <TextField
-            id="outlined-basic"
-            label="Item"
-            variant="outlined"
-            fullWidth
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-          />
-          <Button
-            variant="outlined"
-            onClick={() => {
-              addItem(itemName)
-              setItemName('')
-              handleClose()
-            }}
-          >
-            Add
-          </Button>
-        </Stack>
-        </Box>
+        <Box
+          position="absolute" 
+          top="50%" 
+          left="50%" 
+          transform="translate(-50%,-50%)"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          
+        ></Box>
       </Modal>
       <Typography variant="h1">Inventory Management</Typography>
     </Box>  
